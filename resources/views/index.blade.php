3aee5f224550d8efb16ea7489decfd7035ee975a
ahmdriffai <ahmmd.riffai@gmail.com>
2022-12-14 17:48:04 +0700

    menghilangkan dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    </div>
-
     <!-- Content Row -->
     <div class="row">
         <div class="col-lg-11 col-md-8 col-sm-6">
